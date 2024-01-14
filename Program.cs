d9b0b137db373d057147d810d726dbf8449fbb8f
gregusio <grzegorzdynaq@gmail.com>
2024-01-14 20:18:54 +0100

    add Blazor.Bootstrap

--- Program.cs
+++ Program.cs
@@ -9,12 +9,14 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddBlazorComponents();
 
+builder.Services.AddBlazorBootstrap();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error", true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
