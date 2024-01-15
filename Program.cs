78caccb1c4ab9d1626ab1d1f5f76adec70477289
Małgorzata Drąg <malgorzata.drag@spoton.com>
2024-01-15 18:14:22 +0100

    Basic site (#1)
    * add BlazorSchool.Components.Web package
    
    * add BlazorSchool.Components.Web package
    
    * simple example
    
    * add elements
    
    * fix compile
    
    * upload file form
    
    * add Blazor.Bootstrap
    
    * move styles to Home.razor.css
    
    * some formatting
    
    * add Blazor.Bootstrap
    
    * redesign the site
    
    * some changes in layout
    
    * minor changes
    
    * new way to change context of site
    
    * change buttons
    
    * json data export
    
    * fix saving data to json
    
    ---------
    
    Co-authored-by: gregusio <grzegorzdynaq@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using BlazorSchool.Components.Web.Core;
 using DoorPrintGenerator.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddBlazorComponents();
+
+builder.Services.AddBlazorBootstrap();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error", true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
