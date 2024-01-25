5c9c7441376fafb12c165ec188d2cae126b310c6
Grzegorz Dynak <77176069+gregusio@users.noreply.github.com>
2024-01-25 23:43:48 +0100

    Add more info to json (#4)
    * add more fields to site info
    
    * add more info to save and load json

--- Components/Pages/Info.cs
+++ Components/Pages/Info.cs
@@ -4,4 +4,18 @@ public class Info
 {
     public string? RoomNumber { get; set; }
     public string? Names { get; set; }
+    
+    public string? RoomNumberFontName { get; set; }
+    public string? RoomNumberFontSize { get; set; }
+    public string? NamesFontName { get; set; }
+    public string? NamesFontSize { get; set; }
+    
+    public double LogoOffsetX { get; set; }
+    public double LogoOffsetY { get; set; }
+    
+    public double RoomNumberOffsetX { get; set; }
+    public double RoomNumberOffsetY { get; set; }
+    
+    public double NamesOffsetX { get; set; }
+    public double NamesOffsetY { get; set; }
 }
